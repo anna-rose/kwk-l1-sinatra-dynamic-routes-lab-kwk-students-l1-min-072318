bb744dc6f035efd7272c27075616bbc798cd115a
anna-rose <917566@isd624.org>
2018-07-31 19:41:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
   num = params[:num].to_i
   @phrase = params[:phrase]
-  ({@phrase}*num)
+  (*num)
   end 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
   word1 = params[:word1]
